99d34ba05a087d8c8019180a346a81de861c1c2f
bera5186 <rbasu611@gmail.com>
2019-07-21 18:21:44 +0530

    Error corrected

--- createDatabase.js
+++ createDatabase.js
@@ -1,1 +1,9 @@
-const mongoClient =
+const mongoClient = require('mongodb'); 
+var url = 'mongodb://localhost:27017/mydb'
+
+mongoClient.connect(url, (error, db) => {
+    if (error) throw error ;
+
+    console.log('Database Created');
+    db.close();
+});
